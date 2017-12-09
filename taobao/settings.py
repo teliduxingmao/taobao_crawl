4d2ed0c403c0c43c1b7ca1a2010ee3f0fff9ae3d
cat <779154427@qq.com>
2017-12-10 02:28:45 +0800

    setting添加了一些参数说明

--- taobao/settings.py
+++ taobao/settings.py
@@ -1,8 +1,11 @@
+﻿#需要搜索的关键词
 KEYWORD = '零食'
+#保存商品信息的mongo数据库的相关信息
 MONGO_URI = 'localhost'
 MONGO_DB = 'taobao'
 MONGO_TB = 'lingshi'
 MONGO_PASSWD = ''
+#保存商品ID的redis数据库的相关信息
 REDIS_URI = 'localhost'
 SET_KEY = 'lingshi-id'
 REDIS_PORT = 6379
